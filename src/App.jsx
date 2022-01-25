37bf6b330c50b0b57ccc21d19ed1db58aa23895e
Hamza Bouyahya <hamzabouyahya198@gmail.com>
2022-01-25 12:44:07 +0100

    get data from remote api with alan ai

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 
 const alanKey =
   "b0f46291f4219359adc1e93331242a3b2e956eca572e1d8b807a3e2338fdd0dc/stage";
-useEffect(() => {
-  alanBtn({
-    key: alanKey,
-    onCommand: ({ command }) => {
-      if (command) {
-      }
-    },
-  });
-}, []);
+
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
   return (
     <div>
       <h1>Alan Ai News application</h1>
