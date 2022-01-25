a362a0e21a9089a027ca8437640ee47ca65b80a9
Hamza Bouyahya <hamzabouyahya198@gmail.com>
2022-01-25 15:31:49 +0100

    news by source with alan ai

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
+import alanImage from "./alan.jpg";
 
 const alanKey =
   "b0f46291f4219359adc1e93331242a3b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-          console.log(articles);
+          setNewsArticles(articles);
         }
       },
     });
   }, []);
   return (
     <div>
-      <h1>Alan Ai News application</h1>
+      <div className={classes.logoContainer}>
+        <img src={alanImage} className={classes.alanLogo} alt="alan logo" />
+      </div>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 }
---
+++ src/components/NewsCard/NewsCard.jsx
@@ -0,0 +1,57 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import useStyles from "./styles";
+
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          image={
+            urlToImage ||
+            "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+          }
+          className={classes.media}
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default NewsCard;
